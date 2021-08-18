e6e6725eab3e8e653abd5265b72707264f43e30f
anabanami <ana.fabelah@gmail.com>
2021-08-18 10:53:36 +1000

    Finished draft for report1, added:complex_array_to_rgb function (currently testing it)

--- code/TIE_test.py
+++ code/TIE_test.py
@@ -16,7 +16,8 @@ os.system(f'rm {folder}/*.png')
 # functions
 def TIE(z, I, Φ):
     # propagating in space
-    return (- 1 / k0) * (2 * ifft(4 * np.pi**2 * (-k ** 2) * fft(I) * fft(Φ)))
+    # print(f"{(- 1 / k0) * (2 * ifft(4 * np.pi**2 * (-k ** 2) * fft(I) * fft(Φ)))}")
+    return np.real((- 1 / k0) * (2 * ifft(4 * np.pi**2 * (-k ** 2) * fft(I) * fft(Φ))))
 
 def δ(x, z):
     # refractive index: constant inside the cylinder but zero everywhere else
@@ -31,17 +32,6 @@ def dΨ_dz(z, Ψ):
     I, Φ = Ψ
     dI_dz = TIE(z, I, Φ)
     dΦ_dz = -k0 * δ(x, z)
-    # # print(f"\n{np.shape(dΦ_dz[:]) = }\n") # this returns: np.shape(δ_array) = (2048,)
-    # # test PLOT #
-    # # how much should this grow per z?
-    # plt.plot(x, dΦ_dz[:], label="dΦ_dz") 
-    # plt.xlabel("x")
-    # plt.ylabel("dΦ_dz")
-    # plt.legend()
-    # plt.title(f"dΦ_dz(x) for z = {z}")
-    # plt.savefig(folder/f'z ={z}.png')
-    # # plt.show()
-    # plt.clf()
     return np.array([dI_dz, dΦ_dz])
 
 def Runge_Kutta(z, delta_z, Ψ):
@@ -49,30 +39,35 @@ def Runge_Kutta(z, delta_z, Ψ):
     # z is single value
     # Ψ is array with shape: (2, 2048)
     # print(f"\n{np.shape(Ψ) = }\n")
+    k1 = dΨ_dz(z, Ψ)
+    k2 = dΨ_dz(z + delta_z / 2, Ψ + k1 * delta_z / 2)
+    k3 = dΨ_dz(z + delta_z / 2, Ψ + k2 * delta_z / 2)
+    k4 = dΨ_dz(z + delta_z, Ψ + k3 * delta_z)
+    return Ψ + (delta_z / 6) * (k1 + 2 * k2 + 2 * k3 + k4) # array shape = (2, 2048)
 
-    k1 = dΨ_dz(z, Ψ) # array
-    # print(f"\n{np.shape(k1) = }\n")
-    k2 = dΨ_dz(z + delta_z / 2, Ψ + k1 * delta_z / 2) # array
-    # print(f"\n{np.shape(k2) = } \n")
-    k3 = dΨ_dz(z + delta_z / 2, Ψ + k2 * delta_z / 2) # array
-    # print(f"\n{np.shape(k3) = }\n")
-    k4 = dΨ_dz(z + delta_z, Ψ + k3 * delta_z) # array
-    # print(f"\n{np.shape(k4) = }\n")
+def complex_array_to_rgb(Ψ, i): #, rmax=None):
+    '''Takes an array of complex numbers and converts it to an array of [r, g, b],
+    where phase gives hue and saturaton/value are given by the absolute value.
+    Especially for use with imshow for complex plots.'''
 
-    # I, Φ = Ψ
-    # # test PLOT #
-    # # how much should this grow per z?
-    # plt.plot(x, Φ[:], label="Φ") 
-    # plt.xlim(-20 * mm, 20 * mm)
-    # plt.xlabel("x")
-    # plt.ylabel("Φ")
-    # plt.legend()
-    # plt.title(f"Φ(x) for z = {z} inside RK loop")
-    # plt.show()
+    I, Φ = Ψ
 
-    # print(f"\n{np.shape(Ψ + (delta_z / 6) * (k1 + 2 * k2 + 2 * k3 + k4)) = }\n")
-    return Ψ + (delta_z / 6) * (k1 + 2 * k2 + 2 * k3 + k4) # array shape (2, 2048)
+    absmax = np.abs(Ψ).max()
+    print(f"\n{absmax = } ")
 
+    hsv = np.zeros(Ψ.shape + (3,), dtype='float')
+    print(f"\n{hsv = }")
+    hsv[:, :, 0] = Φ / (2 * np.pi) % 1
+    print(f"\n{hsv[:, :, 0] = }")
+
+    hsv[:, :, 1] = 1
+    print(f"\n{hsv[:, :, 1] = }")
+
+    hsv[:, :, 2] = np.clip(np.abs(Ψ) / absmax, 0, 1) # I/I.max() #
+    print(f"\n{hsv[:, :, 2] = }")
+
+    rgb = matplotlib.colors.hsv_to_rgb(hsv)
+    return rgb
 
 # -------------------------------------------------------------------------------- #
 
@@ -87,16 +82,16 @@ if __name__ == '__main__':
 
     # X-ray beam parameters
     λ = 0.05166 * nm  # x-rays wavelength
-    k0 = 2 * np.pi / λ
+    k0 = 2 * np.pi / λ # x-rays wavenumber
 
     # For Fourier space
     k = 2 * np.pi * np.fft.fftfreq(n, x_step)
 
     # Propagation & loop parameters
     i = 0
-    z = -x_max * mm # I changed this to match the symmetry of the cylinder instead of starting in the middle
+    z = -x_max
     z_final = x_max
-    delta_z = 0.1 * mm
+    delta_z = 1 * mm # 0.01 * mm
 
     # Cylinder parameters
     R = 12.75 / 2 * mm
@@ -111,34 +106,19 @@ if __name__ == '__main__':
     # Initial state vector
     Ψ = np.array([I, Φ])
 
+    plt.imshow(complex_array_to_rgb(Ψ, i), cmap="gist_rainbow", origin='lower')
+    # plt.savefig(folder/f'{i:04d}.png')
+    plt.show()
+    plt.clf()
     while z < z_final:
-        
-        # I unpack the state vector to visualise the phase
-        I, Φ = Ψ
-        # test PLOT #
-        if i in range(9):
-            plt.plot(x, Φ[:], label="Φ") 
-            plt.xlim(-20 * mm, 20 * mm)
-            plt.xlabel("x")
-            plt.ylabel("Φ")
-            plt.legend()
-            plt.title(f"Φ(x) for z = {z:.4f} inside RK loop")
-            plt.savefig(folder/f'z ={z:.4f}.png')
-            # plt.show()
-            plt.clf()
-
-        if not i % 10:
-            plt.plot(x, Φ[:], label="Φ") 
-            plt.xlim(-20 * mm, 20 * mm)
-            plt.xlabel("x")
-            plt.ylabel("Φ")
-            plt.legend()
-            plt.title(f"Φ(x) for z = {z:.4f} inside RK loop")
-            plt.savefig(folder/f'z ={z:.4f}.png')
-            # plt.show()
-            plt.clf()
 
         print(f"{i = }")
+        # # Simulation frames
+        # plt.imshow(complex_array_to_rgb(Ψ, i), cmap="gist_rainbow", origin='lower')
+        # plt.savefig(folder/f'{i:04d}.png')
+        # # plt.show()
+        # plt.clf()
+
         # spatial evolution step
         Ψ = Runge_Kutta(z, delta_z, Ψ)
         i += 1
@@ -146,16 +126,22 @@ if __name__ == '__main__':
 
     # After the integration occurs I unpack the state vector
     I, Φ = Ψ
+    # Simulation frames
+    plt.imshow(complex_array_to_rgb(Ψ, i), cmap="gist_rainbow", origin='lower')
+    # plt.savefig(folder/f'{i:04d}.png')
+    plt.show()
+    plt.clf()
 
-    ####################### useful ###################################
+    ####################### TESTS ###################################
 
-    # # TEST PLOTS
-    # ### PLAYING AROUND with δ ###
-    # z = np.linspace(0, x_max, 2000, endpoint=False).reshape((2000, 1))
+    # TEST PLOTS
+    ### PLAYING AROUND with δ ###
+    # z = np.linspace(-x_max, x_max, 2000, endpoint=False).reshape((2000, 1))
     # δ_array = δ(x, z)
     # plt.imshow(δ_array, origin='lower')
     # plt.xlabel("x")
     # plt.ylabel("δ")
+    # # plt.legend()
     # plt.show()
 
     ### PLAYING AROUND with dΨ_dz(z, Ψ) ###
@@ -174,11 +160,29 @@ if __name__ == '__main__':
     # dΨ_dz(6.35 * mm, Ψ)
     # dΨ_dz(6.4 * mm, Ψ)
 
+    ### PLAYING AROUND with Φ ###
+        # # I unpack the state vector to visualise the phase
+        # I, Φ = Ψ
+        # # phase test PLOT #
+        # if not i % 100:
+        #     plt.plot(x, Φ[:], label="Φ") 
+        #     plt.xlim(-20 * mm, 20 * mm)
+        #     plt.xlabel("x")
+        #     plt.ylabel("Φ")
+        #     plt.legend()
+        #     plt.title(f"Φ(x) for z = {z:.4f}")
+        #     plt.savefig(folder/f'{i:04d}.png')
+        #     # plt.show()
+        #     plt.clf()
+
+    ### testing if TIE() has non zero imaginary part ###
+    # TIE(z, I, Φ)
 
 
-    # ### ------------ ###
 
-    # constant (I = 1 vs x)??
+    ### ------------ ###
+
+    # # I vs x??
     # plt.plot(x, I)
     # plt.xlabel("x")
     # plt.ylabel("I")
