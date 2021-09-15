a5390aebce9a8244b5df96cbb80f80d3bd7a5a61
anabanami <ana.fabelah@gmail.com>
2021-09-15 20:04:25 +1000

    implemented parameters in sim to match lab apparatus detector resolution

--- code/2_cylinders.py
+++ code/2_cylinders.py
@@ -47,46 +47,54 @@ def globals():
     h = const.h # 6.62607004e-34 * J * s
     c = const.c # 299792458 * m / s
 
+    # Magnification
+    M = 2.5
+    # M = 4.0
+
     # # # Discretisation parameters
     # # x-array parameters
-    n = 1024
-    n_x = n
-    x_max = (n_x / 2) * 5 * um
-    x = np.linspace(-x_max, x_max, n_x, endpoint=False)
-    delta_x = x[1] - x[0]
+    delta_x = 55 * um / M
+    x_max = 35 * mm / M
+    x_min = -x_max
+
+    n_x = int((x_max - x_min) / delta_x)
+    print(f"\n{n_x = }")
+    x = np.linspace(-x_max, x_max, n_x, endpoint=False) 
+   
     # # y-array parameters
-    n_y = n
-    y_max = (n_y / 2) * 5 * um
-    y = np.linspace(-y_max, y_max, n_y, endpoint=False)
-    delta_y = y[1] - y[0]
-    y = y.reshape(n_y, 1)
+    delta_y = 55 * um / M
+    y_max = 7 * mm / M
+    y_min = -y_max
 
-    # # Parameters from X-ray attenuation calculator   
-    # E1 = 22.1629 # keV # Ag k-alpha1 
-    # λ = h * c / (E1 * 1000 * const.eV)
-    # k1 = 2 * np.pi / λ  # x-rays wavenumber
-    # # Material = water, density = 1 g/cm**3
-    # δ1 = 469.337 * nm
-    # μ1 = 64.55083 # per m
-    # β1 = μ1 / (2 * k1)
-    # # Material = ice, density = 0.92 g/cm**3
-    # δ2 = 431.790 * nm
-    # μ2 = 59.38677 # per m
-    # β2 = μ2 / (2 * k1)
+    n_y = int((y_max - y_min) / delta_y)
+    print(f"\n{n_y = }")
+    y = np.linspace(-y_max, y_max, n_y, endpoint=False).reshape(n_y, 1)
 
-    # Optimistic case 
-    E1 = 24 # keV
+    # Parameters from X-ray attenuation calculator   
+    E1 = 22.1629 # keV # Ag k-alpha1 
     λ = h * c / (E1 * 1000 * const.eV)
     k1 = 2 * np.pi / λ  # x-rays wavenumber
-
-    # # Material = gray matter, density = ? g/cm**3
-    δ1 = 459.1 * nm
-    μ1 =  52 # per m
+    # Material = water, density = 1 g/cm**3
+    δ1 = 469.337 * nm
+    μ1 = 64.55083 # per m
     β1 = μ1 / (2 * k1)
-    # # Material = white matter, density = ? g/cm**3
-    δ2 = 426.31 * nm
-    μ2 = 56 # per m
+    # Material = ice, density = 0.92 g/cm**3
+    δ2 = 431.790 * nm
+    μ2 = 59.38677 # per m
     β2 = μ2 / (2 * k1)
+
+    # # Optimistic case 
+    # E1 = 24 # keV
+    # λ = h * c / (E1 * 1000 * const.eV)
+    # k1 = 2 * np.pi / λ  # x-rays wavenumber
+    # # # Material = gray matter, density = ? g/cm**3
+    # δ1 = 459.1 * nm
+    # μ1 =  52 # per m
+    # β1 = μ1 / (2 * k1)
+    # # # Material = white matter, density = ? g/cm**3
+    # δ2 = 426.31 * nm
+    # μ2 = 56 # per m
+    # β2 = μ2 / (2 * k1)
 
     # For Fourier space
     kx = 2 * np.pi * np.fft.fftfreq(n_x, delta_x)
@@ -105,7 +113,7 @@ def globals():
     height = 10 * mm
 
 
-    return x, y, n_x, n_y, delta_x, delta_y, E1, k1, kx, ky, R1, R2, z_c, x_c, δ1, μ1, β1, δ2, μ2, β2, height 
+    return M, x, y, n_x, n_y, delta_x, delta_y, E1, k1, kx, ky, R1, R2, z_c, x_c, δ1, μ1, β1, δ2, μ2, β2, height 
 
 
 # -------------------------------------------------------------------------------- #
@@ -113,9 +121,9 @@ def globals():
 
 if __name__ == '__main__':
 
-    x, y, n_x, n_y, delta_x, delta_y, E1, k1, kx, ky, R1, R2, z_c, x_c, δ1, μ1, β1, δ2, μ2, β2, height = globals()
+    M, x, y, n_x, n_y, delta_x, delta_y, E1, k1, kx, ky, R1, R2, z_c, x_c, δ1, μ1, β1, δ2, μ2, β2, height = globals()
 
-    z_final =  5 * m
+    z_final =  2.5 * m / M # eff propagation distance
 
     T1 = thicc(x, y, R1)
     T2 = thicc(x, y, R2)
@@ -126,9 +134,8 @@ if __name__ == '__main__':
     two_cylinders_δT = δT1 + (δ2 - δ1) * T2
     two_cylinders_βT = βT1 + (β2 - β1) * T2  
 
-    # I = xri.sim.propAS(δT1, βT1, E1, z_final, delta_x, supersample=3)
-    # I = xri.sim.propAS(δT2, βT2, E1, z_final, delta_x, supersample=3)
-    I = xri.sim.propAS(two_cylinders_δT, two_cylinders_βT, E1, z_final, delta_x, supersample=3)
+    print("Propagating Wavefield")
+    I = xri.sim.propAS(two_cylinders_δT, two_cylinders_βT, E1, z_final, delta_x, supersample=6)
 
     # Re-bin step each pixel should now be 20um
     I = zoom(I, 4.0, order=3)
